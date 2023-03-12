d88837c856a6b601ec6aff658c1c0bc2b015c700
Harihar Swaminathan <77826551+harihar-s@users.noreply.github.com>
2023-03-12 19:09:37 +0530

    Update main.cpp
    fixed read

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 // reads value from file into parameters
-void read(string filename, int &n, int &m, int val[], vector<int> adj[]){
+void read(string filename, int &n, int &m, vector<int> &val, vector<int> &A){
 	ifstream inFile;
 	inFile.open(filename, ios::binary | ios::in);
 
@@ -13,17 +13,24 @@ void read(string filename, int &n, int &m, int val[], vector<int> adj[]){
 
 	for(int i = 0; i < n; i++){
 		int deg = 0, tmp;
-		inFile.read(reinterpret_cast<char *>(val + i), 4);
+		inFile.read(reinterpret_cast<char *>(&tmp), 4);
 		inFile.read(reinterpret_cast<char *>(deg), 4);
+		val.push_back(tmp);
 		for(int j = 0; j < deg; j++){
 			inFile.read(reinterpret_cast<char *>(&tmp), 4);
-			adj[i].push_back(tmp);
+			A.push_back(tmp);
 		}
 	}
 	inFile.close();
 }
 
 int main(int argc, char* argv[]){
-	
+	int n, m, i, j;
+	vector<int> val, A;
+
+	read("", n, m, val, A);
+
+	vector<int> adj[n];
+
 	return 0;
 }

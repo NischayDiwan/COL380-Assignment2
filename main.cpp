e49c06f0c582c2beb4dbf1bfdf01393b0aef9d96
Harihar Swaminathan <77826551+harihar-s@users.noreply.github.com>
2023-03-12 21:56:29 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ void read(string filename, int &n, int &m, vector<int> &val, vector<int> &A){
 	for(int i = 0; i < n; i++){
 		int deg = 0, tmp;
 		inFile.read(reinterpret_cast<char *>(&tmp), 4);
-		inFile.read(reinterpret_cast<char *>(deg), 4);
+		inFile.read(reinterpret_cast<char *>(&deg), 4);
 		val.push_back(tmp);
 		for(int j = 0; j < deg; j++){
 			inFile.read(reinterpret_cast<char *>(&tmp), 4);

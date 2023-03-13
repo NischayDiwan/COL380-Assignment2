0458596e8a258fcb85cbbf029f9665566812ff90
Harihar Swaminathan <77826551+harihar-s@users.noreply.github.com>
2023-03-13 21:04:26 +0530

    Update main.cpp
    read parameters

--- main.cpp
+++ main.cpp
@@ -1,36 +1,23 @@
 #include<bits/stdc++.h>
-//#include<mpi.h>
+#include<mpi.h>
 
 using namespace std;
 
-// reads value from file into parameters
-void read(string filename, int &n, int &m, vector<int> &val, vector<int> &A){
-	ifstream inFile;
-	inFile.open(filename, ios::binary | ios::in);
-
-	inFile.read(reinterpret_cast<char *>(&n), 4);
-	inFile.read(reinterpret_cast<char *>(&m), 4);
-
-	for(int i = 0; i < n; i++){
-		int deg = 0, tmp;
-		inFile.read(reinterpret_cast<char *>(&tmp), 4);
-		inFile.read(reinterpret_cast<char *>(&deg), 4);
-		val.push_back(tmp);
-		for(int j = 0; j < deg; j++){
-			inFile.read(reinterpret_cast<char *>(&tmp), 4);
-			A.push_back(tmp);
-		}
-	}
-	inFile.close();
-}
-
 int main(int argc, char* argv[]){
-	int n, m, i, j;
-	vector<int> val, A;
 
-	read("", n, m, val, A);
+	MPI_Init(&argc, &argv);
+	int id, sz;
+	MPI_Comm_rank(MPI_COMM_WORLD, &id);
+	MPI_Comm_size(MPI_COMM_WORLD, &sz);
 
-	vector<int> adj[n];
+	string header = argv[3].substr(13, argv[3].length - 13);	// check 13 ?!
+	string filename = argv[2].substr(12, argv[2].length - 12);
 
+	startk = atoi(argv[6]);
+	endk = atoi(argv[7]);
+
+	// print the above values on sample file	
+
+	MPI_Finalize();
 	return 0;
 }

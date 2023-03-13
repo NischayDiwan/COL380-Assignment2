2e3ecae5c4eadbec00be872017a48a9448865be3
Harihar Swaminathan <77826551+harihar-s@users.noreply.github.com>
2023-03-13 21:08:36 +0530

    Update main.cpp
    update parameters

--- main.cpp
+++ main.cpp
@@ -10,8 +10,10 @@ int main(int argc, char* argv[]){
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
 	MPI_Comm_size(MPI_COMM_WORLD, &sz);
 
-	string header = argv[3].substr(13, argv[3].length - 13);	// check 13 ?!
-	string filename = argv[2].substr(12, argv[2].length - 12);
+	string header = argv[3];
+	header = header.substr(13, header.length - 13);	// check 13 ?!
+	string filename = argv[2];
+	string = string.substr(12, string.length - 12);
 
 	int startk = atoi(argv[6]), endk = atoi(argv[7]);
 

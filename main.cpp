f2dd61db0be365db719daab32501455351330d12
Harihar Swaminathan <77826551+harihar-s@users.noreply.github.com>
2023-03-14 21:11:17 +0530

    works on smol case

--- main.cpp
+++ main.cpp
@@ -154,20 +154,19 @@ int main(int argc, char* argv[]){
 		done = 1;
 	}
 	MPI_Allreduce(&done, &action, 1, MPI_INT, MPI_PROD, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
 	action = 1 - action;
 
 	while(action){
 		vector<pair<int, int>> cur;
-		int min = 100;
+		int min = INT_MAX;
 		if(active.size()!=0){
 			min = (*active.begin()).first;
 		}
-		int global_min = 100;
+		int global_min = INT_MAX;
 		MPI_Allreduce(&min, &global_min, 1, MPI_INT, MPI_MIN, MPI_COMM_WORLD);
 
 		if(!done){
-			if(active.size() == 0)
-				done = 1;
 			set<pair<int, pair<int, int>>>::iterator itr1 = active.begin(), itr2 = active.begin();
 			for(; itr2 != active.end(); itr2++){
 				if((*itr2).first > global_min){
@@ -181,9 +180,12 @@ int main(int argc, char* argv[]){
 				}
 			}
 			active.erase(itr1, itr2);
-			
+			if(active.size() == 0)
+				done = 1;
 		}
+
 		vector<pair<pair<int, int>, int>> W;
+		set<pair<pair<int, int>, int>> Y;
 		for(auto e: cur){
 			for(auto w: supp[{e.first, e.second}]){
 				W.push_back({e, w});
@@ -192,7 +194,7 @@ int main(int argc, char* argv[]){
 		int ptr = 0;
 		while(true){
 			// dst1 for (u, w) edge  and   dst2 for (v, w) edge
-			int b[7], u = 0, v = 0, w = 0, dst1 = 0, dst2 = 0;
+			int b[9], u = 0, v = 0, w = 0, dst1 = 0, dst2 = 0;
 			if(ptr >= W.size()){
 				b[0] = 0;
 			}
@@ -205,38 +207,42 @@ int main(int argc, char* argv[]){
 					dst1 = u % sz;
 					b[1] = u;
 					b[2] = w;
+					b[3] = v;
 				}
 				else{
 					dst1 = w %sz;
 					b[1] = w;
 					b[2] = u;
+					b[3] = v;
 				}
 				if(prio[v] < prio[w]){
 					dst2 = v % sz;
-					b[4] = v;
-					b[5] = w;
+					b[5] = v;
+					b[6] = w;
+					b[7] = u;
 				}
 				else{
 					dst2 = w % sz;
-					b[4] = w;
-					b[5] = v;
+					b[5] = w;
+					b[6] = v;
+					b[7] = u;
 				}
-				b[3] = dst1; b[6] = dst2;
+				b[4] = dst1; b[8] = dst2;
 			}
 			MPI_Request msreq;
-			MPI_Isend(b, 7, MPI_INT, 0, id, MPI_COMM_WORLD, &msreq);
+			MPI_Isend(b, 9, MPI_INT, 0, id, MPI_COMM_WORLD, &msreq);
 			int num_packets[sz], payload = 0;
 			for(i = 0; i < sz; i++){
 				num_packets[i] = 0;
 			}
-			int mb[7*sz];
+			int mb[9*sz];
 			if(id == 0){
 				for(i = 0; i < sz; i++){
 					MPI_Status status;
-					MPI_Recv(mb+7*i, 7, MPI_INT, i, i, MPI_COMM_WORLD, &status);
-					if(mb[7*i] != 0){
-						num_packets[mb[7*i+3]]++;
-						num_packets[mb[7*i+6]]++;
+					MPI_Recv(mb+9*i, 9, MPI_INT, i, i, MPI_COMM_WORLD, &status);
+					if(mb[9*i] != 0){
+						num_packets[mb[9*i+4]]++;
+						num_packets[mb[9*i+8]]++;
 					}
 				}
 				for(i = 0; i < sz; i++){
@@ -250,28 +256,35 @@ int main(int argc, char* argv[]){
 
 			if(id == 0){
 				for(i = 0; i < sz; i++){
-					if(mb[7*i] == 1){
+					if(mb[9*i] == 1){
 						MPI_Request req1, req2;
-						MPI_Isend(mb+7*i+1, 2, MPI_INT, mb[7*i+3], mb[7*i+3], MPI_COMM_WORLD, &req1);
-						MPI_Isend(mb+7*i+4, 2, MPI_INT, mb[7*i+6], mb[7*i+6], MPI_COMM_WORLD, &req2);
+						MPI_Isend(mb+9*i+1, 3, MPI_INT, mb[9*i+4], mb[9*i+4], MPI_COMM_WORLD, &req1);
+						MPI_Isend(mb+9*i+5, 3, MPI_INT, mb[9*i+8], mb[9*i+8], MPI_COMM_WORLD, &req2);
 					}
 				}
 			}
 
-			vector<pair<int, int>> proc;
-			int rec[2];
+			vector<pair<pair<int, int>, int>> proc;
+			int rec[3];
 			for(i = 0; i < payload; i++){
 				MPI_Status stat;
-				MPI_Recv(rec, 2, MPI_INT, 0, id, MPI_COMM_WORLD, &stat);
-				proc.push_back({rec[0], rec[1]});
+				MPI_Recv(rec, 3, MPI_INT, 0, id, MPI_COMM_WORLD, &stat);
+				proc.push_back({{rec[0], rec[1]}, rec[2]});
 			}
 			
-			for(auto e: proc){
+			for(auto x: proc){
+				pair<int, int> e = x.first;
+				int arr[] = {e.first, e.second, x.second};
+				sort(arr, arr+3);
 				if(hashtable.find(e) != hashtable.end()){
+					//cout << arr[0] << " " << arr[1] << " " << arr[2] << endl;
+					if(Y.find({{arr[0], arr[1]}, arr[2]}) != Y.end())
+						continue;
 					auto erase_itr = active.find({hashtable[e], e});
 					active.erase(erase_itr);
 					hashtable[e]--;
 					active.insert({hashtable[e], e});
+					Y.insert({{arr[0], arr[1]}, arr[2]});
 				}
 			}
 
@@ -280,11 +293,13 @@ int main(int argc, char* argv[]){
 			// break out when all have mb[7k] = 0
 			int b_break = 0;
 			MPI_Allreduce(&b[0], &b_break, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+			MPI_Barrier(MPI_COMM_WORLD);
 			if(b_break == 0)
 				break;
 		}
 
 		MPI_Allreduce(&done, &action, 1, MPI_INT, MPI_PROD, MPI_COMM_WORLD);
+		MPI_Barrier(MPI_COMM_WORLD);
 		action = 1 - action;
 	}
 
@@ -297,8 +312,8 @@ int main(int argc, char* argv[]){
 
 	for(auto truss: T){
 		pair<int, int> e = truss.first;
-		cout << "edge " << e.first << " " << e.second << " has truss number " << e.second;
-		cout << endl;
+		//cout << "edge " << e.first << " " << e.second << " has truss number " << truss.second;
+		//cout << endl;
 	}
 	return 0;
 }
